2dafa5a800065adcf2301c5a19441994244075d2
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-11-28 01:11:54 +0300

    Add 1 Stack Challenge

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,11 +8,33 @@ public class Main {
         System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
         System.out.println(checkForPalindrome("I did, did I?"));
         System.out.println(checkForPalindrome("hello"));
-        System.out.println(checkForPalindrome("Don't node"));
-
+        System.out.println(checkForPalindrome("Racecar"));
+        System.out.println(checkForPalindrome("Don't nod"));
     }
 
     public static boolean checkForPalindrome(String text) {
-        return false;
+        LinkedList<Character> stack = new LinkedList<>();  // stack de açsam bu instance ı fark etmezdi. LinkedList stack gibi çalışır. başa ekler sondan çıkarır.
+        StringBuilder noPunctions = new StringBuilder();
+        String newLowerCasedText = text.toLowerCase(Locale.ENGLISH);
+
+        for(int i=0; i< newLowerCasedText.length(); i++){
+            char x = newLowerCasedText.charAt(i);  // String 'in i. karakteri, array gibi string elemanlarını geziniyor.
+            if(x >= 'a' && x <= 'z') {
+                noPunctions.append(x);
+                stack.push(x);
+                System.out.println(stack);
+            }
+        }
+        StringBuilder reverses = new StringBuilder();
+        while (!stack.isEmpty()) {
+            reverses.append(stack.pop()); // Başından poplar(siler)
+            System.out.println(stack);
+        }
+
+        return noPunctions.toString().contentEquals(reverses);
+        // return noPunctions.toString().equals(reverses.toSting());
     }
 }
+
+// The java.util.LinkedList.pop() method is used to remove and return the top element from the stack represented by the LinkedList. The method simply pops out an element present at the top of the stack. This method is similar to removeFirst method in LinkedList. Syntax:
+//LinkedListObject.pop()
